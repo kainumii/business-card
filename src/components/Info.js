0d299c464e026d69f216e7cfcb54ad69bfc0614d
Miikka Kainu <miikka.kainu@siili.com>
2023-09-06 08:59:02 +0300

    test

---
+++ src/components/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Info = () => {
+  return (
+    <>
+      <Typography align="center" variant="h6">
+        Miikka Kainu
+      </Typography>
+      <Typography align="center" variant="body2" sx={{ color: "#ad674e" }}>
+        Fullstack Developer | React | C# .Net | Agile
+      </Typography>
+    </>
+  );
+};
+
+export default Info;
